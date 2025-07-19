17f0319e87678771e2546b1e40e9240827f13683
Rafael <rtoresani@gmail.com>
2025-07-19 14:38:07 -0300

    add: get schedule config

--- src/modules/schedule/dtos/schedule-config-day.response.ts
+++ src/modules/schedule/dtos/schedule-config-day.response.ts
@@ -3,6 +3,7 @@ import { EWeekDay } from "generated/prisma";
 export class ScheduleConfigDayResponse {
   id: string;
   scheduleId: string;
+  isActive: boolean;
   weekday: EWeekDay;
   startTime: number;
   endTime: number;
